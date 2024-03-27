721addfdc029a971232e0c905496c9644d993520
dinevsg <dinevsg@gmail.com>
2024-03-28 01:54:53 +0200

    Remove files that should be ignored

--- rentify/cars/mixins.py
+++
@@ -1,9 +0,0 @@
-class SlugMixin:
-    def __init__(self):
-        self.kwargs = None
-
-    def get_slug(self):
-        slug = self.kwargs["slug"]
-        return slug
-
-
--- rentify/vanilla/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from rentify.vanilla.views import IndexView
-
-urlpatterns = [
-    path("", IndexView.as_view(), name="index")
-]
